bd87e1550d57a38619ce26d3e5d77153ff1adb44
pablovin <pablovin@gmail.com>
2024-12-25 16:35:23 +0000

    Deploying to gh-pages from @ pablovin/pablovin.github.io@dfb2758d3b4964d7a37eab0fa913240d136c804f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-publications",
           title: "publications",
-          description: "You can filter my publications using keywords.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
